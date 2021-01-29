800eaca7325b630949ac6225299108869f21e22a
Felipe Martins <felipemartins1359@outlook.com>
2021-01-29 16:33:49 -0300

    Lux added

--- WSTowersOffice.Api/App_Start/BundleConfig.cs
+++ WSTowersOffice.Api/App_Start/BundleConfig.cs
@@ -23,8 +23,7 @@ namespace WSTowersOffice.Api
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap-lux.css"));
         }
     }
 }
