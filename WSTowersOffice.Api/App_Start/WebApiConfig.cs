9bf520167835a08bb83c0b7bcd34e26a3fe8dc96
Juan Douglas <juandouglas2004@gmail.com>
2021-02-02 13:43:46 -0300

    New Connection String method

--- WSTowersOffice.Api/App_Start/WebApiConfig.cs
+++ WSTowersOffice.Api/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.Entity.Core.EntityClient;
 using System.Linq;
 using System.Web.Http;
 using WSTowersOffice.Api.Properties;
@@ -9,7 +10,18 @@ namespace WSTowersOffice.Api
 {
     public static class WebApiConfig
     {
-        public static string EntityFreameworkConnectionString { get { return $"metadata=res://*/Models.WSTowersOfficeModel.csdl|res://*/Models.WSTowersOfficeModel.ssdl|res://*/Models.WSTowersOfficeModel.msl;provider=System.Data.SqlClient;provider connection string=&quot;{ConnectionString}&quot;"; } }
+        public static string[] KeyValueConnectionString { get => ConnectionString.Split(';'); }
+        public static string EntityFreameworkConnectionString { get {
+                var connectionBuilder = new EntityConnectionStringBuilder
+                {
+                    Provider = "System.Data.SqlClient",
+                    ProviderConnectionString = $"{ConnectionString};MultipleActiveResultSets=True;App=EntityFramework;",
+                    Metadata = @"res://*/Models.WSTowersOfficeModel.csdl|res://*/Models.WSTowersOfficeModel.ssdl|res://*/Models.WSTowersOfficeModel.msl"
+                };
+                string connectionString = connectionBuilder.ToString();
+                return connectionString;
+            } 
+        }
         public static string ConnectionString { get { return ConfigurationManager.ConnectionStrings["WSTowersOfficeEntities"].ConnectionString; } }
         public static void Register(HttpConfiguration config)
         {
