a0907170385e06ab8db1b30c577252fedadbc102
= <juandouglas2004@gmail.com>
2021-02-08 16:44:23 -0300

    AdminsController add

---
+++ WSTowersOffice.Api/Controllers/AdminsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WSTowersOffice.Api.Models;
+
+namespace WSTowersOffice.Api.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private WSTowersOfficeEntities db = new WSTowersOfficeEntities();
+
+        // GET: Admins
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Login.ToListAsync());
+        }
+
+        // GET: Admins/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = await db.Login.FindAsync(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // GET: Admins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admins/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ID,UserName,Password,Email,isValidEmail,CreateDate,ValidKey")] Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Login.Add(login);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(login);
+        }
+
+        // GET: Admins/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = await db.Login.FindAsync(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // POST: Admins/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ID,UserName,Password,Email,isValidEmail,CreateDate,ValidKey")] Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(login).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(login);
+        }
+
+        // GET: Admins/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = await db.Login.FindAsync(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // POST: Admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Login login = await db.Login.FindAsync(id);
+            db.Login.Remove(login);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
