aff16dcd17ac6031998f8f2f60a5629c1e07beb3
= <juandouglas2004@gmail.com>
2021-03-19 11:59:47 -0300

    Api Execution Save

---
+++ WSTowersOffice.Execution.Api/Models/EmployeeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WSTowersOffice.Execution.Api.Models
+{
+    public class EmployeeModel
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "CPF")]
+        [StringLength(15, MinimumLength = 11)]
+        public string CPF { get; set; }
+        [Required]
+        [Display(Name = "Nome")]
+        [StringLength(500, MinimumLength = 3)]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Pessoas na família")]
+        public int QuantityFamilyPersons { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        [StringLength(1000, MinimumLength = 4)]
+        public string Email { get; set; }
+        [Display(Name = "Imagem de perfil")]
+        public FileModel ProfileImage { get; set; }
+    }
+}
---
+++ WSTowersOffice.Execution.Api/Models/FileModel.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Xml.Serialization;
+
+namespace WSTowersOffice.Execution.Api.Models
+{
+    public class FileModel
+    {
+        public int ID { get; set; }
+        public int Leaght { get; set; }
+        public string Filename { get; set; }
+        public FileType FileType { get; set; }
+        private UriBuilder UriBuilder => new UriBuilder(System.Web.HttpContext.Current.Request.Url.AbsoluteUri);
+        [JsonIgnore]
+        [XmlIgnore]
+        public string ImageUrl
+        {
+            get
+            {
+                UriBuilder builder = UriBuilder;
+                builder.Path = "api/Files/Image";
+                builder.Query = $"filename={Filename}&filetype={FileType}&extension={ImageExtension.png}";
+                return builder.ToString();
+            }
+        }
+        public FileModel(File file)
+        {
+            if (file != null)
+            {
+                Leaght = file.Leaght;
+                Filename = file.FileName;
+                FileType = (FileType)file.FileType;
+                ID = file.ID;
+            }
+        }
+        public FileModel()
+        {
+        }
+    }
+}
